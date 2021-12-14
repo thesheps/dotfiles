32291be7c7b45507d18d06091f3a9ebc084dd8cf
Chris Shepherd <thesheps@gmail.com>
2021-12-14 18:46:36 +0000

    feat(zsh): adds powerlevel10k theme

--- install.sh
+++ install.sh
@@ -2,5 +2,8 @@
 
 sudo apt update
 sudo apt upgrade
-sudo apt install zsh
+sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+gh repo clone romkatv/powerlevel10k "$ZSH_CUSTOM/themes/powerlevel10k"
+git clone https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
