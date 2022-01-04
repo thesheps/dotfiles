fa640be38feee2b6fb661362c934b5729a3eb40d
Chris Shepherd <thesheps@gmail.com>
2022-01-04 21:45:38 +0000

    feat: home dir corrections

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ elif [ "$(uname)" == "Linux" ]; then
     sudo apt install zsh git-all -q
 fi
 
-git clone https://github.com/thesheps/dotfiles "$HOME/.dotfiles"
+if cd "$HOME/.dotfiles"; then git pull; else git clone https://github.com/thesheps/dotfiles "$HOME/.dotfiles"; fi
 sh "$HOME/.dotfiles/bootstrap.sh"
