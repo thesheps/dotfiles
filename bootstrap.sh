75a29260b049e9b0fd0530b6823c21917095a5a5
Chris Shepherd <thesheps@gmail.com>
2022-01-04 21:32:15 +0000

    feat: adds zsh autosuggestions

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 
 ln -sf "$HOME/.dotfiles/.p10k.zsh" "$HOME/.p10k.zsh"
 ln -sf "$HOME/.dotfiles/.zshrc" "$HOME/.zshrc"
 
-chsh -s "$(which zsh)" pi
+chsh -s "$(which zsh)"
 exec zsh
